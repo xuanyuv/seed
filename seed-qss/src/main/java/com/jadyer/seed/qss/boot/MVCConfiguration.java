b57f335d8e5341bf5e9c02dd52ffab833284a28d
jadyer <jadyer@yeah.net>
2017-03-06 10:51:29 +0800

    [^] 10

--- seed-qss/src/main/java/com/jadyer/seed/qss/boot/MVCConfiguration.java
+++ seed-qss/src/main/java/com/jadyer/seed/qss/boot/MVCConfiguration.java
@@ -9,16 +9,6 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 public class MVCConfiguration extends WebMvcConfigurerAdapter {
-	//@Bean
-	//public EmbeddedServletContainerCustomizer containerCustomizer(){
-	//	return new EmbeddedServletContainerCustomizer() {
-	//		@Override
-	//		public void customize(ConfigurableEmbeddedServletContainer container) {
-	//			container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/WEB-INF/jsp/common/404.jsp"));
-	//			container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/WEB-INF/jsp/common/500.jsp"));
-	//		}
-	//	};
-	//}
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("forward:/qss/list");
