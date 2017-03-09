8d21900f920b3273341349e1c6af534796d71808
jadyer <jadyer@yeah.net>
2017-03-10 04:56:15 +0800

    [^] 46

---
+++ seed-simulator/src/main/java/com/jadyer/seed/simulator/base/AuthFilter.java
@@ -0,0 +1,76 @@
+package com.jadyer.seed.simulator.base;
+
+import com.jadyer.seed.comm.constant.Constants;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 权限验证
+ * <p>
+ *     Session超时后，Ajax请求会被"Status Code:302 Found"到超时后的登录页面
+ *     此时前台js中收到的Ajax结果是undefined，其实是没有得到应答，所以alert(jsonData.message)时会弹出undefined
+ * </p>
+ * Created by 玄玉<https://jadyer.github.io/> on 2014/11/3 10:39.
+ */
+public class AuthFilter extends OncePerRequestFilter {
+    private String url;
+    private List<String> anonymousList = new ArrayList<>();
+
+    AuthFilter(String url, List<String> anonymousList){
+        this.url = url;
+        this.anonymousList = anonymousList;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        /*
+		 * http://121.199.22.212/JadyerWeb/user/get/2?uname=玄玉
+		 * 121.199.22.212是服务器的IP，119.85.113.63是我的IP，即我本地访问服务器接口时本地的IP
+         * 下面列出的是HttpServletRequest属性的控制台输出，输出值前后手工增加了[]
+		 */
+        //fullURL=request.getRequestURI() + (null==request.getQueryString()?"":"?"+request.getQueryString());
+		//fullURL=[/JadyerWeb/user/get/2?uname=%E7%8E%84%E7%8E%89]
+		//request.getRequestURI()=[/JadyerWeb/user/get/2]
+		//request.getQueryString()=[uname=%E7%8E%84%E7%8E%89]
+		//request.getContextPath()=[/JadyerWeb]
+		//request.getServletPath()=[/user/get/2]
+		//request.getRequestURL()=[http://121.199.22.212/JadyerWeb/user/get/2
+		//request.getScheme()=[http]
+		//request.getServerName()=[121.199.22.212]
+		//request.getServerPort()=[80]
+		//request.getLocalAddr()=[121.199.22.212]
+		//request.getLocalName()=[AY140324164327Z]
+		//request.getLocalPort()=[80]
+		//request.getRemoteAddr()=[119.85.113.63]
+		//request.getRemoteHost()=[119.85.113.63]
+		//request.getRemotePort()=[18458]
+		//request.getRemoteUser()=[null]
+		//request.getRequestedSessionId()=[0812467359CA7599CDF09AD780F0804A]
+        /*
+         * 增加对[/js/**]模式的资源控制
+         */
+        boolean disallowAnonymousVisit = true;
+        for(String anonymousResource : anonymousList){
+            if(anonymousResource.equals(request.getServletPath())){
+                disallowAnonymousVisit = false;
+                break;
+            }
+            if(anonymousResource.endsWith("/**") && request.getServletPath().startsWith(anonymousResource.replace("/**", ""))){
+                disallowAnonymousVisit = false;
+                break;
+            }
+        }
+        if(disallowAnonymousVisit && null==request.getSession().getAttribute(Constants.WEB_SESSION_USER)){
+            response.sendRedirect(request.getContextPath() + this.url);
+        }else{
+            filterChain.doFilter(request, response);
+        }
+    }
+}
---
+++ seed-simulator/src/main/java/com/jadyer/seed/simulator/util/LogUtil.java
@@ -0,0 +1,36 @@
+package com.jadyer.seed.simulator.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 日志工具类
+ * <p>完整版见https://github.com/jadyer/JadyerEngine/blob/master/JadyerEngine-common/src/main/java/com/jadyer/engine/common/util/LogUtil.java</p>
+ * @version v2.3
+ * @history v2.3-->修复获取logger方法中，可能会获取到其它线程绑定的logger，导致获取到的不是想要的logger
+ * @history v2.2-->优化Log获取为显式指定所要获取的Log,未指定时默认取上一次的Log,没有上一次的则取defaultLog
+ * @history v2.1-->新增多线程情景下的日志集中打印功能
+ * @history v2.0-->新增日志的数据库保存和邮件发送功能
+ * @history v1.0-->通过<code>java.lang.ThreadLocal</code>实现日志记录器
+ * @update Aug 26, 2015 3:29:21 PM
+ * @create Dec 18, 2012 6:19:31 PM
+ * @author 玄玉<https://jadyer.github.io/>
+ */
+public final class LogUtil {
+	private static final String LOGGER_NAME_APP = "appLogger";
+
+	private LogUtil(){}
+
+	private static ThreadLocal<Logger> currentLoggerMap = new ThreadLocal<>();
+
+	private static Logger appLogger = LoggerFactory.getLogger(LOGGER_NAME_APP);
+
+	public static Logger getAppLogger() {
+		Logger logger = currentLoggerMap.get();
+		if(null!=logger && LOGGER_NAME_APP.equals(logger.getName())){
+			return logger;
+		}
+		currentLoggerMap.set(appLogger);
+		return appLogger;
+	}
+}
