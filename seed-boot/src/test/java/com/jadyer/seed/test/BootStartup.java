d44c0b982935c3a64e61d8b59bbd54e1e2c5498b
jadyer <jadyer@yeah.net>
2017-03-09 23:38:30 +0800

    [^] 39

--- seed-boot/src/test/java/com/jadyer/seed/test/BootStartup.java
+++ seed-boot/src/test/java/com/jadyer/seed/test/BootStartup.java
@@ -1,10 +1,22 @@
 package com.jadyer.seed.test;
 
 import com.jadyer.seed.boot.BootStrap;
+import com.jadyer.seed.boot.event.ApplicationEnvironmentPreparedEventListener;
+import com.jadyer.seed.boot.event.ApplicationFailedEventListener;
+import com.jadyer.seed.boot.event.ApplicationPreparedEventListener;
+import com.jadyer.seed.boot.event.ApplicationStartedEventListener;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class BootStartup {
     public static void main(String[] args) {
-        new SpringApplicationBuilder().sources(BootStrap.class).profiles("local").run(args);
+        //SpringApplication.run(BootStarp.class, args);
+        //new SpringApplicationBuilder().sources(BootStrap.class).profiles("local").run(args);
+        new SpringApplicationBuilder().sources(BootStrap.class)
+                .listeners(new ApplicationStartedEventListener())
+                .listeners(new ApplicationEnvironmentPreparedEventListener())
+                .listeners(new ApplicationPreparedEventListener())
+                .listeners(new ApplicationFailedEventListener())
+                .profiles("local")
+                .run(args);
     }
 }
