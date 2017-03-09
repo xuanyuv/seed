cf1fe466cfac4ead50e93391e42df028e03bb470
jadyer <jadyer@yeah.net>
2017-03-09 23:28:08 +0800

    [^] 38

--- seed-open/src/main/java/com/jadyer/seed/open/boot/OpenBootStrap.java
+++ seed-open/src/main/java/com/jadyer/seed/open/boot/OpenBootStrap.java
@@ -1,8 +1,6 @@
 package com.jadyer.seed.open.boot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +8,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.Filter;
 
-@SpringBootApplication(scanBasePackages="${scan.base.packages}", exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages="${scan.base.packages}")
 public class OpenBootStrap extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
