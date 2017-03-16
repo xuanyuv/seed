ad2f6c986ec6ad9b8b7aef33a0d2c0eb9f7ead57
jadyer <jadyer@yeah.net>
2017-03-16 20:14:09 +0800

    [^] 79

---
+++ seed-server/src/main/java/com/jadyer/seed/server/MainApp.java
@@ -0,0 +1,13 @@
+package com.jadyer.seed.server;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 玄玉服务器启动类
+ * Created by 玄玉<https://jadyer.github.io/> on 2013/9/3 20:14.
+ */
+public class MainApp {
+	public static void main(String[] args) {
+		new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+}
