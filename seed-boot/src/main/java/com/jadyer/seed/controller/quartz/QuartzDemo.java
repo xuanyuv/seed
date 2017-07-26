f80864019c655f44b366164409447aa98054537e
jadyer <jadyer@yeah.net>
2017-07-26 16:02:55 +0800

    [^] 140

--- seed-boot/src/main/java/com/jadyer/seed/controller/quartz/QuartzDemo.java
+++ seed-boot/src/main/java/com/jadyer/seed/controller/quartz/QuartzDemo.java
@@ -1,8 +1,13 @@
 package com.jadyer.seed.controller.quartz;
 
+import com.jadyer.seed.comm.util.JadyerUtil;
+import com.jadyer.seed.comm.util.LogUtil;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by 玄玉<http://jadyer.cn/> on 2017/7/8 9:00.
@@ -13,5 +18,15 @@ public class QuartzDemo {
     @Scheduled(cron="${spring.quartz.cron.demo}")
     void justDoIt(){
         System.out.println("quartz-demo-" + System.currentTimeMillis());
+        List<String> dataList = Arrays.asList("1", "2", "3", "4", "5", "6");
+        String currIndex = "1";
+        int len = dataList.size();
+        LogUtil.getLogger().info("定时任务：xxxx-->查到记录[{}]条", len);
+        for(String obj : dataList){
+            currIndex = JadyerUtil.leftPadUseZero(currIndex, String.valueOf(len).length());
+            LogUtil.getLogger().info("读取到数据-->[{}]", obj);
+            LogUtil.getLogger().info("定时任务：xxxx-->处理完毕[{}-{}]条", len, currIndex);
+            currIndex = String.valueOf(Integer.parseInt(currIndex) + 1);
+        }
     }
 }
