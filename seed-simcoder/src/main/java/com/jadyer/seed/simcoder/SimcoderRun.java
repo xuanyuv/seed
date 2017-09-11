096733669f439661e16b2e80fa3f0b85a668cb42
jadyer <jadyer@yeah.net>
2017-09-11 16:46:53 +0800

    [^] 178

--- seed-simcoder/src/main/java/com/jadyer/seed/simcoder/SimcoderRun.java
+++ seed-simcoder/src/main/java/com/jadyer/seed/simcoder/SimcoderRun.java
@@ -4,6 +4,7 @@ import com.jadyer.seed.simcoder.helper.GenerateHelper;
 
 public class SimcoderRun {
     public static void main(String[] args){
-        GenerateHelper.generate("mpp", "t_mpp_user_info");
+        //GenerateHelper.generate("mpp", "t_mpp_user_info");
+        GenerateHelper.generate("mpp");
     }
 }
