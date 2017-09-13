845ca37c1e7a728cccec72cbd6252946967ca9fb
jadyer <jadyer@yeah.net>
2017-09-13 14:51:16 +0800

    [^] 192

--- seed-seedoc/src/main/java/com/jadyer/seed/seedoc/boot/BeetlConfiguration.java
+++ seed-seedoc/src/main/java/com/jadyer/seed/seedoc/boot/BeetlConfiguration.java
@@ -3,6 +3,7 @@ package com.jadyer.seed.seedoc.boot;
 import org.beetl.core.resource.ClasspathResourceLoader;
 import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
 import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,18 +18,19 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class BeetlConfiguration {
-    @Bean(initMethod="init")
+    @Bean(initMethod="init", name="beetlConfig")
     public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
         BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
-        beetlGroupUtilConfiguration.setResourceLoader(new ClasspathResourceLoader("templates"));
+        beetlGroupUtilConfiguration.setResourceLoader(new ClasspathResourceLoader(BeetlConfiguration.class.getClassLoader(), "templates/"));
         return beetlGroupUtilConfiguration;
     }
 
-    @Bean
-    public BeetlSpringViewResolver getBeetlSpringViewResolver(BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+    @Bean(name="beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(@Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
         BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
-        beetlSpringViewResolver.setContentType("text/html; charset=UTF-8");
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
         beetlSpringViewResolver.setViewNames("**/*.html");
+        beetlSpringViewResolver.setSuffix(".html");
         beetlSpringViewResolver.setOrder(0);
         beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
         return beetlSpringViewResolver;
