798de0b4188286fda8ec97cda9c9162bb395325c
jadyer <jadyer@yeah.net>
2017-10-25 15:19:38 +0800

    [^] 228

--- seed-mpp/src/main/java/com/jadyer/seed/mpp/sdk/weixin/model/pay/WeixinPayCloseorderRespData.java
+++ seed-mpp/src/main/java/com/jadyer/seed/mpp/sdk/weixin/model/pay/WeixinPayCloseorderRespData.java
@@ -7,4 +7,12 @@ package com.jadyer.seed.mpp.sdk.weixin.model.pay;
 public class WeixinPayCloseorderRespData extends WeixinPayRespData {
     /** 业务结果描述（对于业务执行的详细描述） */
     private String result_msg;
+
+    public String getResult_msg() {
+        return result_msg;
+    }
+
+    public void setResult_msg(String result_msg) {
+        this.result_msg = result_msg;
+    }
 }
