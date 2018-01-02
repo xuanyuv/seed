76cc30572c4778d7253d7fd4c90515c2d35a0f16
jadyer <jadyer@yeah.net>
2018-01-02 16:15:56 +0800

    [^] 238

---
+++ seed-boot/src/main/java/com/jadyer/seed/controller/tmp/elasticjob/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 本目录下的demo为：待整理
+ * Comment by 玄玉<http://jadyer.cn/> on 2018/1/2 16:14.
+ */
+package com.jadyer.seed.controller.tmp.elasticjob;
---
+++ seed-comm/src/main/java/com/jadyer/seed/comm/util/tmp/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 本目录下工具类为：待整理
+ * Comment by 玄玉<http://jadyer.cn/> on 2018/1/2 16:13.
+ */
+package com.jadyer.seed.comm.util.tmp;
