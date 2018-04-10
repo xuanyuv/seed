da8f8c7be186a6e0877f6440d3f2ee06f4ba63b3
jadyer <jadyer@yeah.net>
2018-04-10 17:46:19 +0800

    [^] 255

--- seed-simcoder/src/main/java/com/jadyer/seed/simcoder/SimcoderRun.java
+++ seed-simcoder/src/main/java/com/jadyer/seed/simcoder/SimcoderRun.java
@@ -5,12 +5,12 @@ import com.jadyer.seed.simcoder.helper.GenerateHelper;
 public class SimcoderRun {
     public static final String PACKGET_PREFIX = "com.jadyer.seed.mpp";
     public static final String DB_ADDRESS = "127.0.0.1:3306";
-    public static final String DB_NAME = "ifs";
+    public static final String DB_NAME = "mpp";
     public static final String DB_USERNAME = "root";
     public static final String DB_PASSWORD = "xuanyu";
 
     public static void main(String[] args){
-        //GenerateHelper.generate("mpp", "t_mpp_user_info");
-        GenerateHelper.generate("mpp");
+        //GenerateHelper.generate(DB_NAME, "t_mpp_user_info", "t_mpp_fans_info");
+        GenerateHelper.generate(DB_NAME);
     }
 }
