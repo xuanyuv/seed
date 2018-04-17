8487bd72309cd2a8b9f5fffb098f7a9bacef7bf2
jadyer <jadyer@yeah.net>
2018-04-17 16:05:44 +0800

    [^] 257

--- seed-comm/src/main/java/com/jadyer/seed/comm/log/LogAspect.java
+++ seed-comm/src/main/java/com/jadyer/seed/comm/log/LogAspect.java
@@ -1,7 +1,6 @@
 package com.jadyer.seed.comm.log;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.jadyer.seed.comm.constant.CodeEnum;
 import com.jadyer.seed.comm.exception.SeedException;
 import com.jadyer.seed.comm.log.annotation.EnableAutoValid;
@@ -30,11 +29,19 @@ class LogAspect implements MethodInterceptor {
         long startTime = System.currentTimeMillis();
         //类名.方法名（不含类的包名），举例：MppController.bind
         String methodInfo = invocation.getThis().getClass().getSimpleName() + "." + invocation.getMethod().getName();
-        //获取入参
+        //打印入参
         Object[] objs = invocation.getArguments();
-        log.info("{}()被调用，入参为{}", (methodInfo), printArgs(objs));
+        List<Object> argsList = new ArrayList<>();
+        for(Object obj : objs){
+            if(obj instanceof ServletRequest || obj instanceof ServletResponse){
+                continue;
+            }
+            argsList.add(obj);
+        }
+        log.info("{}()被调用，入参为{}", methodInfo, JSON.toJSONStringWithDateFormat(argsList, JSON.DEFFAULT_DATE_FORMAT));
         //表单验证
         for(Object obj : objs){
+            //if(null!=obj && obj.getClass().getName().startsWith("com.jadyer.seed.open.model")){
             if(obj.getClass().isAnnotationPresent(EnableAutoValid.class)){
                 String validateResult = ValidatorUtil.validate(obj);
                 log.info("{}()的表单-->{}", methodInfo, StringUtils.isBlank(validateResult)?"验证通过":"验证未通过");
@@ -45,26 +52,22 @@ class LogAspect implements MethodInterceptor {
         }
         //执行方法
         respData = invocation.proceed();
+        //打印出参
         long endTime = System.currentTimeMillis();
         String returnInfo;
-        if(null!=respData && respData.getClass().isAssignableFrom(ResponseEntity.class)){
+        if(null == respData){
+            returnInfo = "";
+        }else if(respData instanceof ServletRequest) {
+            returnInfo = "ServletRequest";
+        }else if(respData instanceof ServletResponse) {
+            returnInfo = "ServletResponse";
+        }else if(respData.getClass().isAssignableFrom(ResponseEntity.class)) {
             returnInfo = "ResponseEntity";
         }else{
-            returnInfo = JSON.toJSONStringWithDateFormat(respData, JSON.DEFFAULT_DATE_FORMAT, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullListAsEmpty, SerializerFeature.WriteNullBooleanAsFalse);
+            returnInfo = JSON.toJSONStringWithDateFormat(respData, JSON.DEFFAULT_DATE_FORMAT);
         }
         log.info("{}()被调用，出参为{}，Duration[{}]ms", methodInfo, returnInfo, endTime-startTime);
         log.info("---------------------------------------------------------------------------------------------");
         return respData;
-    }
-
-    private String printArgs(Object... objs) {
-        List<Object> argsList = new ArrayList<>();
-        for(Object obj : objs){
-            if(obj instanceof ServletResponse || obj instanceof ServletRequest){
-                continue;
-            }
-            argsList.add(obj);
-        }
-        return JSON.toJSONStringWithDateFormat(argsList, JSON.DEFFAULT_DATE_FORMAT, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullListAsEmpty, SerializerFeature.WriteNullBooleanAsFalse);
     }
 }
