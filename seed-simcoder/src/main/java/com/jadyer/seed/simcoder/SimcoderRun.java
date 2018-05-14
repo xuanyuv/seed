88a2be46b69dbf5e00ef5e94129e6115f83c985d
jadyer <jadyer@yeah.net>
2018-05-14 14:03:16 +0800

    [^] 289

--- seed-simcoder/src/main/java/com/jadyer/seed/simcoder/SimcoderRun.java
+++ seed-simcoder/src/main/java/com/jadyer/seed/simcoder/SimcoderRun.java
@@ -6,7 +6,7 @@ import com.jadyer.seed.simcoder.helper.GenerateHelper;
  * Seed代码生成器
  * ----------------------------------------------------------------------------------------------------------------
  * 1. 目前支持的数据库类型，详见{@link com.jadyer.seed.simcoder.helper.DBHelper#buildJavatypeFromDbtype(String)}
- * 2. 生成的Service中分页方法的条件，暂时只支持int、String两种Java类型的属性（Date和BigDecimal无法判断拿什么作为分页条件）
+ * 2. 生成的Service中分页方法的条件，暂时只支持Integer、Long、String三种属性（Date和BigDecimal无法判断拿什么作为分页条件）
  * ----------------------------------------------------------------------------------------------------------------
  */
 public class SimcoderRun {
