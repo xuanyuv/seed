889221afcf0da8f3de7ba6e191917fffde2c2a13
jadyer <jadyer@yeah.net>
2018-06-07 15:32:25 +0800

    [^] 310

--- seed-boot/src/main/java/com/jadyer/seed/boot/lock/cluster/SeedLockConfiguration.java
+++ seed-boot/src/main/java/com/jadyer/seed/boot/lock/cluster/SeedLockConfiguration.java
@@ -11,6 +11,7 @@ import org.redisson.RedissonRedLock;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
@@ -28,18 +29,25 @@ import java.util.List;
 /**
  * Seed分布式锁处理器
  * ---------------------------------------------------------------------------------------------------
+ * Redisson官方讲解：https://github.com/redisson/redisson/wiki/目录
+ * Redisson实例配置：https://github.com/redisson/redisson/wiki/2.-配置方法#26-单redis节点模式
+ * ---------------------------------------------------------------------------------------------------
  * 1、@SpringBootApplication(scanBasePackages="com.jadyer.seed")
  * 2、引入"org.redisson:redisson:3.7.1"
  * 3、配置文件配置以下属性
  *    redisson:
+ *      lockWatchdogTimeout: 10000
+ *      connectionMinimumIdleSize: 16
+ *      connectionPoolSize: 32
+ *      connectTimeout: 3000
  *      password: xuanyu
  *      nodes:
- *        - 192.168.2.210:7000
- *        - 192.168.2.210:7001
- *        - 192.168.2.210:7002
- *        - 192.168.2.210:7003
- *        - 192.168.2.210:7004
- *        - 192.168.2.210:7005
+ *        - redis://192.168.2.210:7000
+ *        - redis://192.168.2.210:7001
+ *        - redis://192.168.2.210:7002
+ *        - redis://192.168.2.210:7003
+ *        - redis://192.168.2.210:7004
+ *        - redis://192.168.2.210:7005
  * 4、@SeedLock("#userMsg.name")
  *    public CommResult<Map<String, Object>> prop(int id, UserMsg userMsg){ // do business... }
  * ---------------------------------------------------------------------------------------------------
@@ -51,24 +59,55 @@ import java.util.List;
 @ConditionalOnClass({RedissonClient.class})
 @ConfigurationProperties(prefix="redisson")
 public class SeedLockConfiguration {
-    private String password;
+    /** 节点地址：[host:port] */
     private List<String> nodes = new ArrayList<>();
+    /** 监控锁的看门狗超时，单位：毫秒（默认值：30000） */
+    private int lockWatchdogTimeout;
+    /** 最小空闲连接数（默认值：32） */
+    private int connectionMinimumIdleSize;
+    /** 连接池大小（默认值：64） */
+    private int connectionPoolSize;
+    /** 连接超时，单位：毫秒（默认值：10000） */
+    private int connectTimeout;
+    /** 数据库编号（默认值：0） */
+    private int database;
+    /** 密码（默认值：null） */
+    private String password;
     private static final List<RedissonClient> redissonClientList = new ArrayList<>();
     private ExpressionParser parser = new SpelExpressionParser();
     private LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
 
     @PostConstruct
     public void initRedissonClientList(){
-        for (String node : nodes) {
+        //空判断：防止接入SpringCloud后二次初始化
+        if(!redissonClientList.isEmpty()){
+            return;
+        }
+        for(String node : nodes){
             Config config = new Config();
-            if(StringUtils.isBlank(password)){
-                config.useSingleServer().setAddress("redis://" + node);
-            }else{
-                config.useSingleServer().setAddress("redis://" + node).setPassword(password);
+            if(lockWatchdogTimeout > 0){
+                config.setLockWatchdogTimeout(lockWatchdogTimeout);
+            }
+            SingleServerConfig singleConfig = config.useSingleServer().setAddress(node);
+            if(connectionMinimumIdleSize > 0){
+                singleConfig.setConnectionMinimumIdleSize(connectionMinimumIdleSize);
+            }
+            if(connectionPoolSize > 0){
+                singleConfig.setConnectionPoolSize(connectionPoolSize);
+            }
+            if(connectTimeout > 0){
+                singleConfig.setConnectTimeout(connectTimeout);
+            }
+            if(database > 0){
+                singleConfig.setDatabase(database);
+            }
+            if(StringUtils.isNotBlank(password)){
+                singleConfig.setPassword(password);
             }
             redissonClientList.add(Redisson.create(config));
         }
     }
+
 
     @Around("@annotation(com.jadyer.seed.boot.lock.cluster.SeedLock)")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -117,19 +156,55 @@ public class SeedLockConfiguration {
     }
 
 
+    public List<String> getNodes() {
+        return nodes;
+    }
+
+    public int getLockWatchdogTimeout() {
+        return lockWatchdogTimeout;
+    }
+
+    public void setLockWatchdogTimeout(int lockWatchdogTimeout) {
+        this.lockWatchdogTimeout = lockWatchdogTimeout;
+    }
+
+    public int getConnectionMinimumIdleSize() {
+        return connectionMinimumIdleSize;
+    }
+
+    public void setConnectionMinimumIdleSize(int connectionMinimumIdleSize) {
+        this.connectionMinimumIdleSize = connectionMinimumIdleSize;
+    }
+
+    public int getConnectionPoolSize() {
+        return connectionPoolSize;
+    }
+
+    public void setConnectionPoolSize(int connectionPoolSize) {
+        this.connectionPoolSize = connectionPoolSize;
+    }
+
+    public int getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public int getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(int database) {
+        this.database = database;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public List<String> getNodes() {
-        return nodes;
-    }
-
-    public static List<RedissonClient> getRedissonClientList() {
-        return redissonClientList;
     }
 }
