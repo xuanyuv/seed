7ef9d037d8b519372489b5669822ddb560f1c880
jadyer <jadyer@yeah.net>
2018-07-23 19:31:38 +0800

    [^] 330

--- seed-comm/src/main/java/com/jadyer/seed/comm/util/BeanUtil.java
+++ seed-comm/src/main/java/com/jadyer/seed/comm/util/BeanUtil.java
@@ -7,10 +7,10 @@ import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -25,12 +25,67 @@ import java.util.Map;
 public final class BeanUtil {
     private BeanUtil(){}
 
+    public static <E, T> List<T> copyPropertiesForList(List<E> sourceList, Class<T> targetClass) {
+        List<T> targetList = new ArrayList<>();
+        for(E e : sourceList){
+            targetList.add(copyProperties(e, targetClass));
+        }
+        return targetList;
+    }
+
+
+    /**
+     * JavaBean属性拷贝
+     * -------------------------------------------------------------------------------------
+     * 1、cglib 和 Spring 就足够了，Apache的还是算了
+     * 2、使用时注意目标类应public，若内部类则应为静态内部类
+     * -------------------------------------------------------------------------------------
+     */
+    public static <E, T> T copyProperties(E source, Class<T> targetClass) {
+        T target;
+        try {
+            target = targetClass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("copyProperties时发生异常：目标类非public（内部类则应static），堆栈轨迹如下", e);
+        }
+        assert target != null;
+        ////采用Cglib实现（实际使用时可以在Service类中全局缓存BeanCopier对象）
+        //net.sf.cglib.beans.BeanCopier beanCopier = BeanCopier.create(source.getClass(), target.getClass(), false);
+        //beanCopier.copy(source, target, null);
+        //采用Spring实现
+        org.springframework.beans.BeanUtils.copyProperties(source, target);
+        ////采用反射实现
+        //try {
+        //    for (Method sourceMethod : source.getClass().getMethods()) {
+        //        if (!sourceMethod.getName().startsWith("get") && !sourceMethod.getName().startsWith("is")) {
+        //            continue;
+        //        }
+        //        //得到源对象的Setter
+        //        String sourceFieldName;
+        //        if (sourceMethod.getName().startsWith("get")) {
+        //            sourceFieldName = "set" + sourceMethod.getName().substring(3);
+        //        } else {
+        //            sourceFieldName = "set" + sourceMethod.getName().substring(2);
+        //        }
+        //        try {
+        //            targetClass.getMethod(sourceFieldName, sourceMethod.getReturnType()).invoke(target, sourceMethod.invoke(source));
+        //        } catch (NoSuchMethodException e) {
+        //            //ignore exception
+        //        }
+        //    }
+        //} catch (IllegalAccessException | InvocationTargetException e) {
+        //    throw new RuntimeException("copyProperties失败，堆栈轨迹如下", e);
+        //}
+        return target;
+    }
+
+
     /**
      * HttpServletRequest参数值转为JavaBean
-     * --------------------------------------------------------------------------------------
+     * -------------------------------------------------------------------------------------
      * 1、类属性只能是String
      * 2、只能处理当前类，暂不能处理父类和子类
-     * --------------------------------------------------------------------------------------
+     * -------------------------------------------------------------------------------------
      */
     public static <T> T requestToBean(HttpServletRequest request, Class<T> beanClass){
         try{
@@ -40,77 +95,18 @@ public final class BeanUtil {
             for(Field field : beanClass.getDeclaredFields()){
                 //构造setter方法
                 String methodName = "set" + StringUtils.capitalize(field.getName());
-                try{
-                    //执行setter方法
-                    String fieldValue = request.getParameter(field.getName());
-                    fieldValue = null==fieldValue ? "" : fieldValue;
+                //执行setter方法
+                String fieldValue = request.getParameter(field.getName());
+                fieldValue = null==fieldValue ? "" : fieldValue;
+                try {
                     beanClass.getMethod(methodName, String.class).invoke(bean, URLDecoder.decode(fieldValue, "UTF-8"));
-                }catch(Exception e){
+                } catch (NoSuchMethodException e) {
                     //ignore exception
                 }
             }
             return bean;
         }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    /**
-     * 采用反射实现的JavaBean属性拷贝
-     * <ul>
-     *     <li>实测拷贝效率由低到高依次为（最快的目前是cglib，也是推荐采用的）</li>
-     *     <li>net.sf.cglib.beans.BeanCopier.copy()</li>
-     *     <li>org.springframework.beans.BeanUtils.copyProperties()</li>
-     *     <li>com.jadyer.seed.comm.util.BeanUtil.copyProperties()</li>
-     *     <li>org.apache.commons.beanutils.BeanUtils.copyProperties()</li>
-     * </ul>
-     * <p>
-     *     曾试过优化一下BeanCopier，把这个对象放到全局的ConcurrentHashMap<String, BeanCopier>里面<br>
-     *     放进去的beanCopier对象就是BeanCopier.create(source.getClass(), target.getClass(), false)<br>
-     *     意味着只要是从相同的source拷贝属性给target，就不用每次create()，而是直接从ConcurrentHashMap中取<br>
-     *     不过测试发现，放到ConcurrentHashMap之后的效率反倒不如每次都BeanCopier.create()，以后有时间再研究吧
-     * </p>
-     * <p>
-     *     另外，这里自己写的反射获取Setter有点复杂了好像，可以参考上面的requestToBean()方法<br>
-     *     以后真正用的时候再改，今天没心情......
-     * </p>
-     * <ul>
-     *     <li>注意：实际使用发现，属性值为Long类型时无法拷贝，属性值为long类型时可以拷贝</li>
-     * </ul>
-     */
-    public static <T, E> void copyProperties(T source, E target){
-        //采用Cglib实现
-        //net.sf.cglib.beans.BeanCopier beanCopier = BeanCopier.create(source.getClass(), target.getClass(), false);
-        //beanCopier.copy(source, target, null);
-        //采用Spring实现
-        //org.springframework.beans.BeanUtils.copyProperties(source, target);
-        //采用反射实现
-        Method[] sourceMethods = source.getClass().getDeclaredMethods();
-        Method[] targetMethods = target.getClass().getDeclaredMethods();
-        for(Method sourceMethod : sourceMethods){
-            if(sourceMethod.getName().startsWith("get") || sourceMethod.getName().startsWith("is")){
-                //得到源对象的Setter
-                String sourceFieldName;
-                if(sourceMethod.getName().startsWith("get")){
-                    sourceFieldName = "set" + sourceMethod.getName().substring(3);
-                }else{
-                    sourceFieldName = "set" + sourceMethod.getName().substring(2);
-                }
-                for(Method targetMethod : targetMethods){
-                    if(targetMethod.getName().equals(sourceFieldName)){
-                        //参数类型和返回类型判断
-                        if(sourceMethod.getReturnType().isAssignableFrom(targetMethod.getParameterTypes()[0])){
-                            try {
-                                targetMethod.invoke(target, sourceMethod.invoke(source));
-                                break;
-                            } catch (IllegalAccessException | InvocationTargetException e) {
-                                throw new RuntimeException("属性拷贝失败", e);
-                            }
-                        }
-                    }
-                }
-            }
+            throw new RuntimeException("requestToBean发生异常，堆栈轨迹如下", e);
         }
     }
 
--- seed-comm/src/test/java/com/jadyer/seed/test/BeanUtilTest.java
+++ seed-comm/src/test/java/com/jadyer/seed/test/BeanUtilTest.java
@@ -4,48 +4,73 @@ import com.jadyer.seed.comm.util.BeanUtil;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeanUtilTest {
-    /**
-     * 通过反射实现的属性拷贝方法测试
-     */
     @Test
-    public void beanCopyPropertiesTest(){
+    public void beanCopyPropertiesTest() {
         UserDetail user11 = new UserDetail();
-        UserDetail user22 = new UserDetail();
-        user11.setId(12);
+        user11.setId(12L);
         user11.setName("我是玄玉");
         user11.setSex("male");
-        long startTime = System.currentTimeMillis();
-        for(int i=0; i<10000000; i++){
-            BeanUtil.copyProperties(user11, user22);
+        user11.setSmile(true);
+        System.out.println(ReflectionToStringBuilder.toString(BeanUtil.copyProperties(user11, UserDetail.class)));
+        List<UserDetail> userList = new ArrayList<>();
+        UserDetail user01 = new UserDetail();
+        user01.setId(1L);
+        user01.setName("我是玄玉");
+        user01.setSex("male");
+        user01.setSmile(true);
+        UserDetail user02 = new UserDetail();
+        user02.setId(2L);
+        user02.setName("我是玄玉");
+        user02.setSex("male");
+        user02.setSmile(true);
+        UserDetail user03 = new UserDetail();
+        user03.setId(3L);
+        user03.setName("我是玄玉");
+        user03.setSex("male");
+        user03.setSmile(true);
+        userList.add(user01);
+        userList.add(user02);
+        userList.add(user03);
+        for(UserDetail user : BeanUtil.copyPropertiesForList(userList, UserDetail.class)){
+            System.out.println(ReflectionToStringBuilder.toString(user));
         }
-        System.out.println("耗时[" + (System.currentTimeMillis()-startTime) +"]ms转换完毕，得到" + ReflectionToStringBuilder.toString(user22));
     }
 
 
-    class User{
-        private int id;
+    public static class User {
+        private Long id;
         private String name;
-        public int getId() {
+        public Long getId() {
             return id;
         }
-        void setId(int id) {
+        public void setId(Long id) {
             this.id = id;
         }
         public String getName() {
             return name;
         }
-        void setName(String name) {
+        public void setName(String name) {
             this.name = name;
         }
     }
-    public class UserDetail extends User{
+    public static class UserDetail extends User {
         private String sex;
+        private boolean smile;
         public String getSex() {
             return sex;
         }
         public void setSex(String sex) {
             this.sex = sex;
+        }
+        public boolean isSmile() {
+            return smile;
+        }
+        public void setSmile(boolean smile) {
+            this.smile = smile;
         }
     }
 }
