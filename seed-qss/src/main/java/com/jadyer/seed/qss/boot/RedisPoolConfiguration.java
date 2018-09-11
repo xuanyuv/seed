183dc2e828d489462b98d9fd6e9aab878067ee8d
jadyer <jadyer@yeah.net>
2018-09-11 10:26:38 +0800

    [^] 364

--- seed-qss/src/main/java/com/jadyer/seed/qss/boot/RedisPoolConfiguration.java
+++ seed-qss/src/main/java/com/jadyer/seed/qss/boot/RedisPoolConfiguration.java
@@ -73,7 +73,7 @@ public class RedisPoolConfiguration {
         //}
         //return new ShardedJedisPool(config, nodes);
         //这是传URI的方式，会更简洁一些
-        //URI uri = URI.create("redis://redis:abc@redis.dev1.ctstest.com");
+        //URI uri = URI.create("redis://redis:password@redis.dev1.ctstest.com");
         //JedisPool pool = new JedisPool(config, uri, this.connectionTimeout);
         String[] parts = StringUtils.split(this.nodes.get(0), ":");
         JedisPool pool = new JedisPool(config, parts[0], Integer.parseInt(parts[1]),  this.connectionTimeout, this.password);
