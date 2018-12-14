ba0218c9f8bbfcfaff1f7e2bd962bcbe3956a79a
jadyer <jadyer@yeah.net>
2018-12-14 18:51:19 +0800

    [^] 423

--- seed-comm/src/test/java/com/jadyer/seed/test/BeetlTest.java
+++ seed-comm/src/test/java/com/jadyer/seed/test/BeetlTest.java
@@ -18,17 +18,28 @@ import java.util.Map;
 public class BeetlTest {
     @Test
     public void buildFile() throws IOException {
+        /*
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>beetl demo</title>
+        </head>
+        <body>
+            长篇架空武侠小说：${book}，真的忍心烂尾么？？
+        </body>
+        </html>
+        */
         //使用模板文件位置实例化模板对象
-        GroupTemplate groupTemplate = new GroupTemplate(new FileResourceLoader("C:/Users/hongyu.lu/Desktop/"), Configuration.defaultConfiguration());
+        GroupTemplate groupTemplate = new GroupTemplate(new FileResourceLoader("C:/Users/Jadyer/Desktop/"), Configuration.defaultConfiguration());
         //设置模板所需的共享变量
         Map<String, Object> sharedVars = new HashMap<>();
         sharedVars.put("book", "英雄志");
-        sharedVars.put("name", "观海云远");
         groupTemplate.setSharedVars(sharedVars);
         //模板解析成真实的HTML到文件（注意：模板文件source.html必须存在，生成的文件dest.html不需要存在，beetl会自动创建该文件）
         //注意：模板文件source.html必须存在，dest.html可以存在也可以不存在，不存在时beetl会自动创建并填充内容，存在时beetl会覆盖该文件
-        groupTemplate.getTemplate("source.html").renderTo(FileUtils.openOutputStream(new File("C:/Users/hongyu.lu/Desktop/dest.html")));
+        groupTemplate.getTemplate("source.html").renderTo(FileUtils.openOutputStream(new File("C:/Users/Jadyer/Desktop/dest.html")));
         //生成PDF文件
-        WkhtmltopdfUtil.convert(null, "C:/Users/hongyu.lu/Desktop/dest.html", "C:/Users/hongyu.lu/Desktop/dest.pdf");
+        WkhtmltopdfUtil.convert(null, "C:/Users/Jadyer/Desktop/dest.html", "C:/Users/Jadyer/Desktop/dest.pdf");
     }
 }
