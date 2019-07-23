a6528e1f9a4601810724f812bb28222ae4daaf5d
jadyer <jadyer@yeah.net>
2019-07-23 18:31:00 +0800

    [^] 467

--- seed-boot/src/main/java/com/jadyer/seed/controller/batch/xmlconfig/WriterVoid.java
+++ seed-boot/src/main/java/com/jadyer/seed/controller/batch/xmlconfig/WriterVoid.java
@@ -9,7 +9,7 @@ import java.util.List;
  * 空的Writer：啥都不做
  * Created by 玄玉<https://jadyer.cn/> on 2019/7/22 20:11.
  */
-public class WriterNull implements ItemWriter<T> {
+public class WriterVoid implements ItemWriter<T> {
     @Override
     public void write(List<? extends T> items) {
         //nothing to do
