1875e1ee41d757d5221d2bf8bf49272a7d170784
jadyer <jadyer@yeah.net>
2019-08-21 18:19:18 +0800

    [^] 486

--- seed-comm/src/main/java/com/jadyer/seed/comm/util/MyExcelUtil.java
+++ seed-comm/src/main/java/com/jadyer/seed/comm/util/MyExcelUtil.java
@@ -9,7 +9,8 @@ import java.util.List;
 /**
  * 封装MyExcel：https://github.com/liaochong/myexcel/wiki/Excel-Csv导入
  * -------------------------------------------------------------------------------
- * @version v1.0
+ * @version v1.1
+ * @history v1.1-->读取文件失败时，增加逻辑：修改文件后缀名再重读一次
  * @history v1.0-->初建，并添加读取Excel的方法
  *  * -------------------------------------------------------------------------------
  * Created by 玄玉<https://jadyer.cn/> on 2019/8/15 19:31.
@@ -31,15 +32,22 @@ public class MyExcelUtil {
         if(-1 < skipRows){
             saxExcelReader = saxExcelReader.rowFilter(row -> row.getRowNum() > skipRows);
         }
-        //读文件
         try{
+            //读文件
             dataList = saxExcelReader.read(excelFile);
         }catch (OfficeXmlFileException e){
             //若读取xls时报告格式错误，那就试试重命名为xlsx再读（有的excel文件头是2007版的，但文件名却是.xls结尾）
+            String newFilePath = "unkonwnFile";
             if(excelFile.getName().endsWith(".xls")){
-                excelFile.renameTo(new File(excelFile.getPath() + "x"));
+                newFilePath = excelFile.getPath() + "x";
             }
-            dataList = saxExcelReader.read(excelFile);
+            if(excelFile.getName().endsWith(".xlsx")){
+                newFilePath = excelFile.getPath().substring(0, excelFile.getPath().length()-1);
+            }
+            LogUtil.getLogger().warn("文件读取失败，异常信息为：{}。现尝试修改文件后缀名再重新读取一次，新文件名为：{}", e.getMessage(), newFilePath);
+            File newFile = new File(newFilePath);
+            excelFile.renameTo(newFile);
+            dataList = saxExcelReader.read(newFile);
         }
         return dataList;
     }
