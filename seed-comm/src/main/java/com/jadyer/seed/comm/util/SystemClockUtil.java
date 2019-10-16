d529e6f888d8494a132fea19d7b9ccf1c8bb4b26
jadyer <jadyer@yeah.net>
2019-10-16 18:31:57 +0800

    [^] 517

--- seed-comm/src/main/java/com/jadyer/seed/comm/util/SystemClockUtil.java
+++ seed-comm/src/main/java/com/jadyer/seed/comm/util/SystemClockUtil.java
@@ -12,7 +12,8 @@ import java.util.concurrent.atomic.AtomicLong;
  * System.currentTimeMillis()的调用比new一个普通对象要耗时的多（据称100倍左右），因为它要和系统打一次交道
  * 于是编写此类，它在后台定时更新时钟，JVM退出时，线程自动回收
  * -----------------------------------------------------------------------------------------------
- * @version v1.0
+ * @version v1.1
+ * @history v1.1-->修改更新时钟部分为：自加一（可有效解决时钟回拨问题）
  * @history v1.0-->新建
  * -----------------------------------------------------------------------------------------------
  * Created by 玄玉<https://jadyer.cn/> on 2017/10/10 12:34.
@@ -34,8 +35,10 @@ public enum SystemClockUtil {
             thread.setDaemon(true);
             return thread;
         });
-        // 1 秒后首次执行，之后每 1 秒均自动执行一次
-        scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), 1, 1, TimeUnit.MILLISECONDS);
+        // 1 毫秒后首次执行，之后每 1 毫秒均自动执行一次
+        //scheduler.scheduleAtFixedRate(() -> now.set(System.currentTimeMillis()), 1, 1, TimeUnit.MILLISECONDS);
+        //自加一，防止时钟回拨
+        scheduler.scheduleAtFixedRate(now::incrementAndGet, 1, 1, TimeUnit.MILLISECONDS);
     }
 
 
