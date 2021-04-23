208769964156f629eacd48816829bd3d05a43a07
jadyer <jadyer@yeah.net>
2021-04-23 18:23:55 +0800

    xuanyu-591

--- seed-boot/src/main/java/com/jadyer/seed/boot/JasyptConfiguration.java
+++ seed-boot/src/main/java/com/jadyer/seed/boot/JasyptConfiguration.java
@@ -24,7 +24,7 @@ import org.springframework.core.io.support.ResourcePropertySource;
  * ----------------------------------------------------------------------------------------------
  * 下面演示一下Windows中的加解密
  * 根據http://www.jasypt.org/cli.html提示到http://www.jasypt.org/download.html下載到jasypt-1.9.2-dist.zip
- * 解壓jasypt-1.9.2-dist.zip得到文件夾，并進入目錄為C:\Users\Jadyer\Desktop\jasypt-1.9.2\bin的命令行窗口
+ * 解壓jasypt-1.9.2-dist.zip得到文件夾，并進入目录為C:\Users\Jadyer\Desktop\jasypt-1.9.2\bin的命令行窗口
  * encrypt.bat input=xuanyu password=https://jadyer.cn/ stringOutputType=hexadecimal
  * decrypt.bat input=B9063BE29557963568F929EBBD682B5C password=https://jadyer.cn/ stringOutputType=hexadecimal
  * 注意：每一次生成的密文都是不同的，但都可以解密成xuanyu
