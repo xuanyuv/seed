04e03e9a94439cd1de754ff5aa53dd6bb7f21fe3
jadyer <jadyer@yeah.net>
2021-05-20 17:08:44 +0800

    xuanyu-613

--- seed-open/src/main/java/com/jadyer/seed/open/core/RouterController.java
+++ seed-open/src/main/java/com/jadyer/seed/open/core/RouterController.java
@@ -3,41 +3,34 @@ package com.jadyer.seed.open.core;
 import com.jadyer.seed.open.model.ReqData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/open/router")
 public class RouterController {
+    @GetMapping({"", "/", "/api", "/openapi"})
+    String index(){
+        return "/index";
+    }
+
     /**
      * 访问Open开放文档
      */
     @GetMapping("/apidoc")
     String apidoc(){
-        return "/apidoc";
+        return "/apidoc/index";
     }
 
 
-    /**
-     * 直接访问页面资源
-     * <p>
-     *     可以url传参，比如http://127.0.0.1/view?url=/info/get&id=3，则参数id=3会被放到request中
-     * </p>
-     */
-    @GetMapping("/view")
-    String view(String url, HttpServletRequest request){
-        Map<String, String[]> paramMap = request.getParameterMap();
-        for(Map.Entry<String,String[]> entry : paramMap.entrySet()){
-            if(!"url".equals(entry.getKey())){
-                request.setAttribute(entry.getKey(), entry.getValue()[0]);
-            }
-        }
-        return url;
+    @GetMapping("/apidoc/view/{url}")
+    String apidocView(@PathVariable String url){
+        return "/apidoc/" + url;
     }
 
 
